5b31477a9acaa71402af6d59f947537515cb0d04
Ryan O'Toole <rotoole@gmail.com>
2021-01-03 13:03:47 -0800

    remove typo

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 // deck data for routes
 const cardData = ['test', 'foo', 'man', 'chu', 'bar', 'baz', 'dog', 'cat', 'mouse'];
 
-app.use(cors()); b// enable CORS request from any FQDN
+app.use(cors()); // enable CORS request from any FQDN
 app.listen(port);
 console.log("Express Server Listening on " + port);
 
